e154a96ad877db098d0044f01ab151e4ef17df03
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-02-16 16:17:22 -0800

    create and pass testmethod to confirm words with apostrophes are kept intact and that any spaces behind punctuation are not counted as words.

--- WordCounter.Tests/Models.Tests/WordCounterTest.cs
+++ WordCounter.Tests/Models.Tests/WordCounterTest.cs
@@ -78,5 +78,45 @@ namespace WordCounters.Models.Tests
       // Console.WriteLine(outputArray);
       CollectionAssert.AreEqual(testArray, newTextBase[0]);
     }
+
+    [TestMethod]
+    public void TextBaseArrayToListIncludingApostropheWords_SplitCharArraystoListIncludingApostropheWords_True()
+    {
+      string testText2 = "isn't";
+      char[] testArray2 = testText2.ToLower().ToCharArray();
+      WordCounter TextBaseArray2 = new WordCounter();
+      List<char[]> newTextBase2 = new List<char[]> {};
+
+      char[] divisionTextBaseArray2 = TextBaseArray2.SetGuideWord("Bird-bird, bird. Bird isn't the Word.");
+      List<string> forWords2 = new List<string>();
+      for (int i = 0; i < divisionTextBaseArray2.Length; i++)
+      {
+        string letter = divisionTextBaseArray2[i].ToString();
+        if (letter == ". " || letter == ", " || letter == " ")
+        {
+          string combined2 = string.Join("", forWords2);
+          if (combined2 == " ")
+          {
+            forWords2.Clear();
+            return;
+          }
+          else
+          {
+          newTextBase2.Add(combined2.ToCharArray());
+          Console.WriteLine(combined2);
+          forWords2.Clear();
+          }
+        }
+        else
+        {
+          forWords2.Add(letter);
+          // Console.WriteLine(forWords[0]);
+        }
+      }
+      // char[] outputArray = newTextBase2[0];
+      // Console.WriteLine(outputArray);
+      CollectionAssert.AreEqual(testArray2, newTextBase2[3]);
+    }
+
   }
 }
